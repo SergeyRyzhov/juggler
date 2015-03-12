1bb945575fc56f0cf2fc3bdb461f9e640a05e083
SergeyRyzhov <serega.rf@gmail.com>
2015-03-12 08:47:57 +0300

    Added renumber

--- public/js/viewModels/juggler.Orgraph.js
+++ public/js/viewModels/juggler.Orgraph.js
@@ -1,131 +1,183 @@
 (function(app, $) {
-  ko.components.register('juggler-orgraph', {
-    viewModel: function(params) {
-      this.vertex = ko.observable();
-      this.codeArray = ko.observableArray();
+	ko.components.register('juggler-orgraph', {
+		viewModel: function(params) {
+			this.vertex = ko.observable();
+			this.codeArray = ko.observableArray();
 
-      this.graph = ko.observable({});
-      this.warning = ko.observable();
-      this.code = ko.observable();
+			this.graph = ko.observable({});
+			this.warning = ko.observable();
+			this.code = ko.observable();
 
-      this.codeArray.subscribe(_.bind(function(newValue) {
-        this.code = JSON.stringify(this.codeArray());
+			this.codeArray.subscribe(_.bind(function(newValue) {
+				this.code(JSON.stringify(newValue));
 
-        var vertices = this.nodes(),
-          edges = this.edges();
+				var vertices = this.nodes(),
+					edges = this.edges();
 
-        var maxV = _.max(vertices),
-          minV = _.min(vertices);
+				var maxV = _.max(newValue);
 
-        if (minV != 1 || maxV != vertices.length) {
-          this.warning('Invalid code');
-        } else {
-          this.warning('');
-        }
+				if (maxV > newValue.length + 2) {
+					this.warning('Invalid code');
+				} else {
+					this.warning('');
+				}
 
-      }, this));
+			}, this));
 
-      this.prufer = ko.computed(function() {
-        return ParsePrufer(this.codeArray());
-      }, this);
+			this.nodes = ko.computed(function() {
+				return this.graph().nodes;
+			}, this);
 
-      this.nodes = ko.computed(function() {
-        return this.graph().nodes;
-      }, this);
+			this.edges = ko.computed(function() {
+				return this.graph().edges;
+			}, this);
 
-      this.edges = ko.computed(function() {
-        return this.graph().edges;
-      }, this);
+			this.onEnter = _.bind(function(d, e) {
+				// e.keyCode === 13 && this.add();
+				return true;
+			}, this);
 
-      this.onEnter = _.bind(function(d, e) {
-        // e.keyCode === 13 && this.add();
-        return true;
-      }, this);
+			this.add = _.bind(function() {
+				var v = parseInt(this.vertex());
+				if (v > 0) {
+					this.vertex(undefined);
+					this.codeArray.push(v);
+					this.graph(ParsePrufer(this.codeArray()));
+				} else {
+					this.vertex(0);
+				}
+			}, this);
 
-      this.add = _.bind(function() {
-        var v = parseInt(this.vertex());
-        if (v > 0) {
-          this.vertex(undefined);
-          this.codeArray.push(v);
-          this.graph(ParsePrufer(this.codeArray()));
-        } else {
-          this.vertex(0);
-        }
-      }, this);
+			this.reset = _.bind(function() {
+				this.codeArray([]);
+				this.graph(ParsePrufer([]));
+			}, this);
 
-      this.reset = _.bind(function() {
-        this.codeArray([]);
-        this.graph(ParsePrufer([]));
-      }, this);
+			this.generate = _.bind(function() {
+				var n = parseInt(this.vertex());
+				if (n > 20) {
+					n = 20;
+				}
+				this.codeArray([]);
+				for (var i = 0; i < n - 2; i++) {
+					this.codeArray.push(_.random(1, n));
+				}
+				this.graph(ParsePrufer(this.codeArray()));
+			}, this);
 
-      this.generate = _.bind(function() {
-        var n = parseInt(this.vertex());
-        if (n > 20) {
-          n = 20;
-        }
-        this.codeArray([]);
-        for (var i = 0; i < n - 2; i++) {
-          this.codeArray.push(_.random(1, n));
-        }
-        this.graph(ParsePrufer(this.codeArray()));
-      }, this);
+			this.renumber = _.bind(function() {
+					function swap(edges, node, newNode) {
+						_.each(edges, function(edge) {
+							if (edge[0] == node) {
+								edge[0] = newNode;
+							} else
 
-      this.renumber = _.bind(function() {
-        var vertices = this.nodes(),
-          edges = this.edges();
+							if (edge[0] == newNode) {
+								edge[0] = node;
+							}
 
-        var minDegree = _.find(vertices, function(vertex) {
-          return _.every(edges, function(edge) {
-            return edge[1] != vertex;
-          });
-        });
+							if (edge[1] == newNode) {
+								edge[1] = node;
+							} else
 
-        console.log(minDegree);
+							if (edge[1] == node) {
+								edge[1] = newNode;
+							}
+						});
+					}
 
-        var maxV = _.max(vertices),
-          minV = _.min(vertices);
+					function swapArray(array, node, newNode) {
+						return _.map(array, function(value) {
+							if (value == node) {
+								return newNode;
+							}
+							if (value == newNode) {
+								return node;
+							}
+							return value;
+						});
+					}
 
-        console.log(maxV);
-        console.log(minV);
 
-      }, this);
+					var vertices = this.nodes(),
+						edges = this.edges();
+					// code = this.codeArray();
+
+					var firstVertex = _.find(vertices, function(vertex) {
+						return _.every(edges, function(edge) {
+							return edge[1] != vertex;
+						});
+					});
+
+					// console.log(firstVertex);
+
+					var maxV = _.max(vertices),
+						minV = _.min(vertices);
+
+					// console.log(maxV);
+					// console.log(minV);
+
+					swap(edges, firstVertex, 1);
+					// code = swapArray(code, firstVertex, 1);
+					firstVertex = 1;
+					for (var i = 1; i <= vertices.length; i++) {
+						var wale = _.filter(edges, function(edge) {
+							return edge[0] == i;
+						});
+
+						// console.log(wale);
+						_.each(wale, function(edge) {
+							swap(edges, edge[1], firstVertex + 1);
+							// code = swapArray(code, edge[1], firstVertex + 1);
+							firstVertex++;
+						});
+					}
+
+					this.graph({
+						nodes: vertices,
+						edges: edges
+					});
+
+					this.codeArray([]);
+				},
+				this);
 
 
-      this.codeArray([1, 4, 5, 5]);
-      this.graph(ParsePrufer(this.codeArray()));
-    },
-    template: {
-      fromUrl: 'orgraph.html',
-      maxCacheAge: 60000
-    }
-  });
+			this.codeArray([1, 4, 5, 5]);
+			this.graph(ParsePrufer(this.codeArray()));
+		},
+		template: {
+			fromUrl: 'orgraph.html',
+			maxCacheAge: 60000
+		}
+	});
 
-  function ParsePrufer(code) {
-    var arr = code.slice(),
-      edges = [],
-      n = arr.length + 2,
-      B = _.range(1, n + 1),
-      nodes = _.range(1, n + 1);
+	function ParsePrufer(code) {
+		var arr = code.slice(),
+			edges = [],
+			n = arr.length + 2,
+			B = _.range(1, n + 1),
+			nodes = _.range(1, n + 1);
 
-    for (var i = 0; i < arr.length; i++) {
-      var minimum = _.min(_.filter(B, function(b) {
-        return !_.contains(arr, b);
-      }));
+		for (var i = 0; i < arr.length; i++) {
+			var minimum = _.min(_.filter(B, function(b) {
+				return !_.contains(arr, b);
+			}));
 
-      edges.push([arr[i], minimum]);
-      arr.shift();
-      i--;
+			edges.push([arr[i], minimum]);
+			arr.shift();
+			i--;
 
-      var index = B.indexOf(minimum);
-      if (index > -1) {
-        B.splice(index, 1);
-      }
-    }
-    edges.push([_.first(B), _.last(B)]);
-    return {
-      nodes: nodes,
-      edges: edges
-    };
-  }
+			var index = B.indexOf(minimum);
+			if (index > -1) {
+				B.splice(index, 1);
+			}
+		}
+		edges.push([_.first(B), _.last(B)]);
+		return {
+			nodes: nodes,
+			edges: edges
+		};
+	}
 
 })(window.juggler = window.juggler || {}, jQuery);
--- public/js/viewModels/juggler.Prufer.js
+++ public/js/viewModels/juggler.Prufer.js
@@ -1,88 +1,108 @@
 (function(app, $) {
-  ko.components.register('juggler-prufer', {
-    viewModel: function(params) {
-      this.vertex = ko.observable();
-      this.codeArray = ko.observableArray([1, 4, 5, 5]);
+	ko.components.register('juggler-prufer', {
+		viewModel: function(params) {
+			this.vertex = ko.observable();
+			this.codeArray = ko.observableArray([1, 4, 5, 5]);
 
-      this.code = ko.computed(function() {
-        return JSON.stringify(this.codeArray());
-      }, this);
 
-      this.prufer = ko.computed(function() {
-        return ParsePrufer(this.codeArray());
-      }, this);
+			this.graph = ko.observable({});
+			this.warning = ko.observable();
+			this.code = ko.observable();
 
-      this.nodes = ko.computed(function() {
-        return this.prufer().nodes;
-      }, this);
+			this.codeArray.subscribe(_.bind(function(newValue) {
+				this.code(JSON.stringify(newValue));
 
-      this.edges = ko.computed(function() {
-        return this.prufer().edges;
-      }, this);
+				var vertices = this.nodes(),
+					edges = this.edges();
 
-      this.onEnter = _.bind(function(d, e) {
-        // e.keyCode === 13 && this.add();
-        return true;
-      }, this);
+				var maxV = _.max(newValue);
 
-      this.add = _.bind(function() {
-        var v = parseInt(this.vertex());
-        if (v > 0) {
-          this.vertex(undefined);
-          this.codeArray.push(v);
-        } else {
-          this.vertex(0);
-        }
-      }, this);
+				if (maxV > newValue.length + 2) {
+					this.warning('Invalid code');
+				} else {
+					this.warning('');
+				}
 
-      this.reset = _.bind(function() {
-        this.codeArray([]);
-      }, this);
+			}, this));
 
-      this.generate = _.bind(function() {
-        var n = parseInt(this.vertex());
-        if (n > 20) {
-          n = 20;
-        }
-        this.codeArray([]);
-        for (var i = 0; i < n - 2; i++) {
-          this.codeArray.push(_.random(1, n));
-        }
-        //this.codeArray(_.shuffle(_.range(1, n - 1)));
-      }, this);
-    },
-    template: {
-      fromUrl: 'prufer.html',
-      maxCacheAge: 60000
-    }
-  });
+			this.nodes = ko.computed(function() {
+				return this.graph().nodes;
+			}, this);
 
-  function ParsePrufer(code) {
-    var arr = code.slice(),
-      edges = [],
-      n = arr.length + 2,
-      B = _.range(1, n + 1),
-      nodes = _.range(1, n + 1);
+			this.edges = ko.computed(function() {
+				return this.graph().edges;
+			}, this);
 
-    for (var i = 0; i < arr.length; i++) {
-      var minimum = _.min(_.filter(B, function(b) {
-        return !_.contains(arr, b);
-      }));
+			this.onEnter = _.bind(function(d, e) {
+				// e.keyCode === 13 && this.add();
+				return true;
+			}, this);
 
-      edges.push([arr[i], minimum]);
-      arr.shift();
-      i--;
+			this.add = _.bind(function() {
+				var v = parseInt(this.vertex());
+				if (v > 0) {
+					this.vertex(undefined);
+					this.codeArray.push(v);
+					this.graph(ParsePrufer(this.codeArray()));
+				} else {
+					this.vertex(0);
+				}
+			}, this);
 
-      var index = B.indexOf(minimum);
-      if (index > -1) {
-        B.splice(index, 1);
-      }
-    }
-    edges.push([_.first(B), _.last(B)]);
-    return {
-      nodes: nodes,
-      edges: edges
-    };
-  }
+			this.reset = _.bind(function() {
+				this.codeArray([]);
+				this.graph(ParsePrufer(this.codeArray()));
+			}, this);
+
+			this.generate = _.bind(function() {
+				var n = parseInt(this.vertex());
+				if (n > 20) {
+					n = 20;
+				}
+				this.codeArray([]);
+				for (var i = 0; i < n - 2; i++) {
+					this.codeArray.push(_.random(1, n));
+				}
+				//this.codeArray(_.shuffle(_.range(1, n - 1)));
+				this.graph(ParsePrufer(this.codeArray()));
+			}, this);
+
+
+			this.codeArray([1, 4, 5, 5]);
+			this.graph(ParsePrufer(this.codeArray()));
+		},
+		template: {
+			fromUrl: 'prufer.html',
+			maxCacheAge: 60000
+		}
+	});
+
+	function ParsePrufer(code) {
+		var arr = code.slice(),
+			edges = [],
+			n = arr.length + 2,
+			B = _.range(1, n + 1),
+			nodes = _.range(1, n + 1);
+
+		for (var i = 0; i < arr.length; i++) {
+			var minimum = _.min(_.filter(B, function(b) {
+				return !_.contains(arr, b);
+			}));
+
+			edges.push([arr[i], minimum]);
+			arr.shift();
+			i--;
+
+			var index = B.indexOf(minimum);
+			if (index > -1) {
+				B.splice(index, 1);
+			}
+		}
+		edges.push([_.first(B), _.last(B)]);
+		return {
+			nodes: nodes,
+			edges: edges
+		};
+	}
 
 })(window.juggler = window.juggler || {}, jQuery);
