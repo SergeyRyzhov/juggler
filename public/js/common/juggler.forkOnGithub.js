ed80ee7d500f7b2ef03bfa06d63d2da6b26505c7
SergeyRyzhov <serega.rf@gmail.com>
2015-06-12 14:29:34 +0300

    File for fork on github

--- public/js/common/juggler.forkOnGithub.js
+++ public/js/common/juggler.forkOnGithub.js
@@ -2,7 +2,10 @@
 (function(app, $) {
   ko.components.register('juggler-forkongithub', {
     viewModel: function(params) {
-      return {text:'Fork me on GitHub', href:'https://github.com/SergeyRyzhov/juggler'}
+      return {
+        text: 'Fork me on GitHub',
+        href: 'https://github.com/SergeyRyzhov/juggler'
+      }
     },
     template: {
       fromUrl: 'forkongithub.html',
