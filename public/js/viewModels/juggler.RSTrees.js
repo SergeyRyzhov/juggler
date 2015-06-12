363f10baf1728d4b9edc5195b14d810c283af88a
SergeyRyzhov <serega.rf@gmail.com>
2015-06-12 14:20:52 +0300

    Added view for RS trees

--- public/js/viewModels/juggler.RSTrees.js
+++ public/js/viewModels/juggler.RSTrees.js
@@ -1,1 +1,11 @@
 //RS tree builder
+(function(app, $) {
+  ko.components.register('juggler-rstrees', {
+    viewModel: function(params) {
+    },
+    template: {
+      fromUrl: 'rstrees.html',
+      maxCacheAge: 60000
+    }
+  });
+})(window.juggler = window.juggler || {}, jQuery);
