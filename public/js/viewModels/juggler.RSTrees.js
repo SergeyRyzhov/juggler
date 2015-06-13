d41783978912533c2911bb10d3db3a46e24ed390
SergeyRyzhov <serega.rf@gmail.com>
2015-06-13 22:29:57 +0300

    Small UI changes

--- public/js/viewModels/juggler.RSTrees.js
+++ public/js/viewModels/juggler.RSTrees.js
@@ -2,6 +2,17 @@
 (function (app, $) {
   ko.components.register('juggler-rstrees', {
     viewModel: function (params) {
+      
+      var k = ko.observable(5);
+      var r = ko.observable(4);
+      var s = ko.observable(2);
+      
+      
+      return {
+        amountNodes:k,
+        nodeSize:r,
+        intersectionSize:s
+      };
     },
     template: {
       fromUrl: 'rstrees.html',
